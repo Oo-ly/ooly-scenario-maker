9152ffea793b78be30d6412dec6850305aac4c33
Vincent Riva <vincent.riva@outlook.fr>
2020-06-10 11:18:57 +0200

    Add emotions to AnnexeSentences

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -19,16 +19,8 @@
         <div class="card mb-2" v-for="(scenarioAnnexe, index) in scenarioAnnexes" :key="index">
           <div class="card-header d-flex align-items-center">
             {{ scenarioAnnexe.name }}
-            <i
-              class="ml-auto fas fa-eye"
-              v-if="scenarioAnnexe.visible"
-              @click.prevent="scenarioAnnexe.visible = !scenarioAnnexe.visible"
-            ></i>
-            <i
-              class="ml-auto fas fa-eye-slash"
-              v-if="!scenarioAnnexe.visible"
-              @click.prevent="scenarioAnnexe.visible = !scenarioAnnexe.visible"
-            ></i>
+            <i class="ml-auto fas fa-eye" v-if="scenarioAnnexe.visible" @click.prevent="scenarioAnnexe.visible = !scenarioAnnexe.visible"></i>
+            <i class="ml-auto fas fa-eye-slash" v-if="!scenarioAnnexe.visible" @click.prevent="scenarioAnnexe.visible = !scenarioAnnexe.visible"></i>
           </div>
           <div :class="`card-body ${scenarioAnnexe.visible ? 'd-block' : 'd-none'}`">
             <div class="row">
@@ -41,7 +33,9 @@
             </div>
             <label>Type</label>
             <select class="form-control mb-3" v-model="scenarioAnnexe.type">
-              <option value="entry">Introduction</option>
+              <option value="entry:positive">Introduction positive</option>
+              <option value="entry:negative">Introduction négative</option>
+              <option value="entry">Introduction neutre</option>
               <option value="exit">Phrase de fin</option>
             </select>
             <label>Oo</label>
@@ -59,16 +53,8 @@
           <div class="card mb-2" v-for="(sentence, index) in scenarioSentences" :key="index">
             <div class="card-header d-flex align-items-center">
               {{ sentence.name }}
-              <i
-                class="ml-auto fas fa-eye"
-                v-if="sentence.visible"
-                @click.prevent="sentence.visible = !sentence.visible"
-              ></i>
-              <i
-                class="ml-auto fas fa-eye-slash"
-                v-if="!sentence.visible"
-                @click.prevent="sentence.visible = !sentence.visible"
-              ></i>
+              <i class="ml-auto fas fa-eye" v-if="sentence.visible" @click.prevent="sentence.visible = !sentence.visible"></i>
+              <i class="ml-auto fas fa-eye-slash" v-if="!sentence.visible" @click.prevent="sentence.visible = !sentence.visible"></i>
             </div>
             <div :class="`card-body ${sentence.visible ? 'd-block' : 'd-none'}`">
               <div class="row">
@@ -89,11 +75,7 @@
                 <div class="col-6 d-flex align-items-center">
                   <div class="form-check mt-4">
                     <label class="form-check-label">
-                      <input
-                        class="form-check-input"
-                        type="checkbox"
-                        v-model="sentence.interaction"
-                      />
+                      <input class="form-check-input" type="checkbox" v-model="sentence.interaction" />
                       Interaction
                     </label>
                   </div>
@@ -105,16 +87,8 @@
                   <div class="card mb-2" v-for="(s, i) in sentence.sentences" :key="i">
                     <div class="card-header d-flex align-items-center">
                       {{ s.name }}
-                      <i
-                        class="ml-auto fas fa-eye"
-                        v-if="s.visible"
-                        @click.prevent="s.visible = !s.visible"
-                      ></i>
-                      <i
-                        class="ml-auto fas fa-eye-slash"
-                        v-if="!s.visible"
-                        @click.prevent="s.visible = !s.visible"
-                      ></i>
+                      <i class="ml-auto fas fa-eye" v-if="s.visible" @click.prevent="s.visible = !s.visible"></i>
+                      <i class="ml-auto fas fa-eye-slash" v-if="!s.visible" @click.prevent="s.visible = !s.visible"></i>
                     </div>
                     <div :class="`card-body ${s.visible ? 'd-block' : 'd-none'}`">
                       <div class="row">
@@ -132,10 +106,7 @@
                     </div>
                   </div>
                   <div class="form-grou mb-0">
-                    <button
-                      class="btn btn-success mt-2"
-                      @click.prevent="addSubSentence(sentence)"
-                    >Ajouter phrase de sortie</button>
+                    <button class="btn btn-success mt-2" @click.prevent="addSubSentence(sentence)">Ajouter phrase de sortie</button>
                   </div>
                 </div>
               </div>
@@ -154,18 +125,18 @@
 </template>
 
 <script>
-import { v4 as uuidv4 } from "uuid";
-import Input from "./Input";
-import draggable from "vuedraggable";
+import { v4 as uuidv4 } from 'uuid';
+import Input from './Input';
+import draggable from 'vuedraggable';
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: { Input, draggable },
   data: () => {
     return {
       scenario: {
         uuid: uuidv4(),
-        name: ""
+        name: '',
       },
       scenarioOos: [],
       scenarioAnnexes: [],
@@ -173,34 +144,34 @@ export default {
       scenarioSentences: [],
       oos: [
         {
-          uuid: "1c115903-1aa5-4216-b438-5e455124f66d",
-          name: "Disc'Oo"
+          uuid: '1c115903-1aa5-4216-b438-5e455124f66d',
+          name: "Disc'Oo",
         },
         {
-          uuid: "23c7e653-f1a6-435c-9f58-24f9efb8c5e1",
-          name: "Cin'Oo'che"
+          uuid: '23c7e653-f1a6-435c-9f58-24f9efb8c5e1',
+          name: "Cin'Oo'che",
         },
         {
-          uuid: "4d8cf885-f681-452f-97a6-5077744eb4be",
-          name: "Inf'Oo"
+          uuid: '4d8cf885-f681-452f-97a6-5077744eb4be',
+          name: "Inf'Oo",
         },
         {
-          uuid: "a10058d0-46e0-4e54-bf54-72bef1ae283f",
-          name: "Y'Oo'ga"
+          uuid: 'a10058d0-46e0-4e54-bf54-72bef1ae283f',
+          name: "Y'Oo'ga",
         },
         {
-          uuid: "ca5f71b5-8dc0-4acd-9d58-3c3035622afe",
-          name: "Végét'Oo"
+          uuid: 'ca5f71b5-8dc0-4acd-9d58-3c3035622afe',
+          name: "Végét'Oo",
         },
         {
-          uuid: "92021a2e-77d3-48b6-bd31-3e86d6792e05",
-          name: "Wh'Oo'w"
+          uuid: '92021a2e-77d3-48b6-bd31-3e86d6792e05',
+          name: "Wh'Oo'w",
         },
         {
-          uuid: "55a7bed3-a9a5-4c6c-9913-d8966d8dfc59",
-          name: "C'Oo'mique"
-        }
-      ]
+          uuid: '55a7bed3-a9a5-4c6c-9913-d8966d8dfc59',
+          name: "C'Oo'mique",
+        },
+      ],
     };
   },
   computed: {
@@ -210,54 +181,54 @@ export default {
           scenario: null,
           sentences: [],
           oos: [],
-          audios: []
+          audios: [],
         };
 
         json.scenario = this.scenario;
         json.oos = this.scenarioOos;
 
-        this.scenarioSentences.forEach(sentence => {
+        this.scenarioSentences.forEach((sentence) => {
           json.sentences.push({
             uuid: sentence.uuid,
             name: sentence.name,
             interaction: sentence.interaction,
             order: sentence.order,
-            scenarioUuid: this.scenario.uuid
+            scenarioUuid: this.scenario.uuid,
           });
         });
 
-        this.scenarioAnnexes.forEach(annexe => {
+        this.scenarioAnnexes.forEach((annexe) => {
           json.audios.push({
             uuid: uuidv4(),
             name: annexe.name,
             url: annexe.path,
             type: annexe.type,
             audibleUuid: this.scenario.uuid,
-            audibleType: "scenario",
-            ooUuid: annexe.ooUuid
+            audibleType: 'scenario',
+            ooUuid: annexe.ooUuid,
           });
         });
 
-        this.scenarioSentences.forEach(sentence => {
+        this.scenarioSentences.forEach((sentence) => {
           json.audios.push({
             uuid: uuidv4(),
             name: sentence.name,
             url: sentence.path,
             type: sentence.type,
             audibleUuid: sentence.uuid,
-            audibleType: "sentence",
-            ooUuid: sentence.ooUuid
+            audibleType: 'sentence',
+            ooUuid: sentence.ooUuid,
           });
 
-          sentence.sentences.forEach(s => {
+          sentence.sentences.forEach((s) => {
             json.audios.push({
               uuid: uuidv4(),
               name: s.name,
               url: s.path,
               type: s.type,
               audibleUuid: sentence.uuid,
-              audibleType: "sentence",
-              ooUuid: s.ooUuid
+              audibleType: 'sentence',
+              ooUuid: s.ooUuid,
             });
           });
         });
@@ -273,10 +244,8 @@ export default {
         this.scenarioSentences = [];
         this.scenarioAnnexes = [];
 
-        object.sentences.forEach(sentence => {
-          const audio = object.audios.find(
-            a => a.audibleUuid === sentence.uuid
-          );
+        object.sentences.forEach((sentence) => {
+          const audio = object.audios.find((a) => a.audibleUuid === sentence.uuid);
 
           this.scenarioSentences.push({
             uuid: sentence.uuid,
@@ -287,25 +256,23 @@ export default {
             ooUuid: audio.ooUuid,
             order: sentence.order,
             visible: false,
-            sentences: []
+            sentences: [],
           });
         });
 
-        object.audios.forEach(audio => {
-          if (audio.audibleType === "scenario") {
+        object.audios.forEach((audio) => {
+          if (audio.audibleType === 'scenario') {
             this.scenarioAnnexes.push({
               uuid: audio.uuid,
               name: audio.name,
               path: audio.url,
               type: audio.type,
               ooUuid: audio.ooUuid,
-              visible: false
+              visible: false,
             });
-          } else if (audio.audibleType === "sentence") {
+          } else if (audio.audibleType === 'sentence') {
             if (audio.type !== null) {
-              const sentence = this.scenarioSentences.find(
-                s => s.uuid === audio.audibleUuid
-              );
+              const sentence = this.scenarioSentences.find((s) => s.uuid === audio.audibleUuid);
 
               sentence.sentences.push({
                 uuid: audio.uuid,
@@ -313,13 +280,13 @@ export default {
                 path: audio.url,
                 type: audio.type,
                 ooUuid: audio.ooUuid,
-                visible: false
+                visible: false,
               });
             }
           }
         });
-      }
-    }
+      },
+    },
   },
   methods: {
     onDragEnd() {
@@ -330,7 +297,7 @@ export default {
     addOo() {
       this.scenarioOos.push({
         scenarioUuid: this.scenario.uuid,
-        ooUuid: null
+        ooUuid: null,
       });
     },
     addAnnexe() {
@@ -340,7 +307,7 @@ export default {
         path: null,
         type: null,
         ooUuid: null,
-        visible: true
+        visible: true,
       });
     },
     addSentence() {
@@ -353,7 +320,7 @@ export default {
         ooUuid: null,
         order: this.scenarioSentences.length,
         visible: true,
-        sentences: []
+        sentences: [],
       });
     },
     addSubSentence(sentence) {
@@ -362,13 +329,13 @@ export default {
         name: null,
         path: null,
         interaction: false,
-        type: "dislike",
+        type: 'dislike',
         ooUuid: null,
         order: sentence.sentences.length,
-        visible: true
+        visible: true,
       });
-    }
-  }
+    },
+  },
 };
 </script>
 
